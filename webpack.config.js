2e3a6d104b4d346ab6f588bf0935ee076b7484df
Andrei Korolev <andrey-asd123@yandex.ru>
2017-02-08 20:13:27 +0300

    Added commonsChunkPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,12 @@ module.exports = {
         }]
     },
 
-    plugins: []
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            minChunks: 3
+        })
+    ]
 };
 
 if (NODE_ENV == 'production') {
