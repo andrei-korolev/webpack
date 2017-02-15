582b5db99c603fad8f68fb55906d2030c747cd9a
Андрей <andrey-asd123@yandex.ru>
2017-02-15 07:20:10 +0300

    Added hash in file name

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const NODE_ENV = process.env.NODE_ENV || 'development',
-      webpack = require('webpack');
+      webpack = require('webpack'),
+      AssetsPlugin = require('assets-webpack-plugin');
+
+function addHash(template, hash) {
+    return NODE_ENV == 'production' ? template.replace(/\.[^.]+$/, `.[${hash}]$&`) : template;
+}
 
 module.exports = {
     context: __dirname + '/scripts/dev',
@@ -13,7 +18,7 @@ module.exports = {
 
     output: {
         path: __dirname + '/scripts',
-        filename: '[name].js',
+        filename: addHash('[name].js', 'chunkhash'),
         library: '[name]'
     },
 
@@ -38,6 +43,9 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin({
             name: 'common',
             minChunks: 2
+        }),
+        new AssetsPlugin({
+            filename: 'assets.json'
         })
     ]
 };
